a415c027884a1d5b177945da18e78a52e57cfa4f
martin-wrede <martin.wrede@targetx.de>
2025-02-24 10:49:43 +0100

    Cloudflare data

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   base: "/reactimagecreator/", // Important for GitHub Pages deployment
   plugins: [react()], // Necessary for React support
   build: {
-    outDir: "build", // Ensures Vite outputs to the correct folder
+    outDir: "dist", // Ensures Vite outputs to the correct folder
   },
 });
